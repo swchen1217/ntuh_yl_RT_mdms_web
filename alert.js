7385362e4966357a3b5d8745e4fa71d927f85d08
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-15 01:20:22 +0800

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,12 @@
+function HideAlert() {
+    $('#alert-primary').hide();
+    $('#alert-success').hide();
+    $('#alert-danger').hide();
+    $('#alert-warning').hide();
+    $('#alert-info').hide();
+
+    $('#alert-success-in').text('');
+    $('#alert-danger-in').text('');
+    $('#alert-warning-in').text('');
+    $('#alert-info-in').text('');
+}
