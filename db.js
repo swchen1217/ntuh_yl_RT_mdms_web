e0555f81c3e8dd935e07ae0929fac1fcadd50d3b
Sz-Wei Chen <swchen1217@gmail.com>
2019-11-01 15:51:28 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,12 @@
-function SyncDeviceTable(needDialog) {
-    $("#dialog_ProgressBar_Sync").modal("show")
+function SyncDeviceTable(needBar) {
+    if (needBar == true)
+        $("#dialog_ProgressBar_Sync").modal("show")
+    var LastSync;
+    if (localStorage.hasOwnProperty("device_tb_LastSync"))
+        LastSync = localStorage.getItem("device_tb_LastSync");
+    else
+        LastSync = "2019-01-01 00:00:00";
     
+
+
 }
