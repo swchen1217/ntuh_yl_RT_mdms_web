64d5562476beb21cbfb27486e1462686474ff7a6
Sz-Wei Chen <swchen1217@gmail.com>
2019-11-01 15:53:38 +0800

    Update db.js

--- db.js
+++ db.js
@@ -6,7 +6,24 @@ function SyncDeviceTable(needBar) {
         LastSync = localStorage.getItem("device_tb_LastSync");
     else
         LastSync = "2019-01-01 00:00:00";
-    
+    // TODO
+    $.ajax({
+        url: "../ntuh_yl_RT_mdms_api/db.php",
+        data: "mode=sync_device_tb_download",
+        type: "POST",
+        success: function (msg) {
+
+        },
+        error: function (xhr) {
+            console.log('ajax er');
+            $.alert({
+                title: '錯誤',
+                content: 'Ajax 發生錯誤',
+                type: 'red',
+                typeAnimated: true
+            });
+        }
+    });
 
 
 }
