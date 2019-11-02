a2519a967efbc43683d93033c506829356bd94aa
Sz-Wei Chen <swchen1217@gmail.com>
2019-11-02 16:54:18 +0800

    Update WebSql.js

--- WebSql.js
+++ WebSql.js
@@ -22,45 +22,45 @@ class WebSql {
 
     db = openDatabase(this.DATABASE_NAME, this.DATABASE_VERSION, 'MDMS DB', 2 * 1024 * 1024);
 
-    constructor(){
-		var device_tb =
-        "CREATE TABLE IF NOT EXISTS device_tb (" +
-        "  `DID` TEXT," +
-        "  `category` TEXT," +
-        "  `model` TEXT," +
-        "  `number` TEXT," +
-        "  `user` TEXT," +
-        "  `position` TEXT," +
-        "  `status` TEXT," +
-        "  `LastModified` TEXT" +
-        ")";
+    constructor() {
+        var device_tb =
+            "CREATE TABLE IF NOT EXISTS device_tb (" +
+            "  `DID` TEXT," +
+            "  `category` TEXT," +
+            "  `model` TEXT," +
+            "  `number` TEXT," +
+            "  `user` TEXT," +
+            "  `position` TEXT," +
+            "  `status` TEXT," +
+            "  `LastModified` TEXT" +
+            ")";
 
-		var position_item_tb =
-        "CREATE TABLE IF NOT EXISTS position_item_tb (" +
-        "  `type` TEXT," +
-        "  `item` TEXT" +
-        ")";
+        var position_item_tb =
+            "CREATE TABLE IF NOT EXISTS position_item_tb (" +
+            "  `type` TEXT," +
+            "  `item` TEXT" +
+            ")";
         this.db.transaction(function (tx) {
             tx.executeSql(device_tb);
             tx.executeSql(position_item_tb);
         });
     }
 
-    inster(tb_name,data_array){
-        var sql="";
-        sql+="INSERT INTO `"+tb_name+"` (";
-        for(var i=0;i<data_array.length;i++){
-            sql+="`"+data_array[i][0]+"`";
-            if(i!=data_array.length-1)
-                sql+=",";
+    inster(tb_name, data_array) {
+        var sql = "";
+        sql += "INSERT INTO `" + tb_name + "` (";
+        for (var i = 0; i < data_array.length; i++) {
+            sql += "`" + data_array[i][0] + "`";
+            if (i != data_array.length - 1)
+                sql += ",";
         }
-        sql+=") VALUES (";
-        for(var i=0;i<data_array.length;i++){
-            sql+="'"+data_array[i][1]+"'";
-            if(i!=data_array.length-1)
-                sql+=",";
+        sql += ") VALUES (";
+        for (var i = 0; i < data_array.length; i++) {
+            sql += "'" + data_array[i][1] + "'";
+            if (i != data_array.length - 1)
+                sql += ",";
         }
-        sql+=")";
+        sql += ")";
 
         //sql="INSERT INTO `device_tb` (`status`,`DID`) VALUES ('test','MDMS.D0001')";
 
@@ -71,30 +71,21 @@ class WebSql {
         });
     }
 
-    select(tb_name,col,require,callback){
-        var sql="";
-        sql+="SELECT ";
-        if(col!="*"){
-            for (var i=0;i<col.length;i++){
-                sql+="`"+col[i]+"`";
-                if(i!=col.length-1)
-                    sql+=",";
+    select(tb_name, col, require, callback) {
+        var sql = "";
+        sql += "SELECT ";
+        if (col != "*") {
+            for (var i = 0; i < col.length; i++) {
+                sql += "`" + col[i] + "`";
+                if (i != col.length - 1)
+                    sql += ",";
             }
-        }else
-            sql+="*";
-        sql+=" FROM "+tb_name+" "+require;
+        } else
+            sql += "*";
+        sql += " FROM " + tb_name + " " + require;
 
-        console.log(sql);
-        
         this.db.transaction(function (tx) {
-            tx.executeSql(sql,[],function (tx,result) {
-                /*console.log(result);
-                console.log(result.rows);
-                console.log(result.rows.length);
-                console.log(result.rows[1]);
-                console.log(result.rows[1]['DID']);
-                console.log(result.rows.item(2));
-                console.log(result.rows.item(2)['DID']);*/
+            tx.executeSql(sql, [], function (tx, result) {
                 callback(result.rows);
             })
         })
